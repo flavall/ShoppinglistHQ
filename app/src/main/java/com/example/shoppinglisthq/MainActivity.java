2f7b338e8d1380b64774185a048a7fdb3af41fd3
flavall <flaval@online.de>
2019-02-28 10:38:37 +0100

    funzt_28_Feb

--- app/src/main/java/com/example/shoppinglisthq/MainActivity.java
+++ app/src/main/java/com/example/shoppinglisthq/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.shoppinglisthq;
 
+import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
@@ -29,8 +32,17 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -97,7 +109,7 @@ public class MainActivity extends AppCompatActivity {
 
         //mit lambda
         mShoppingMemosListView.setOnItemClickListener((parent, view, position, id) -> {
-            ShoppingMemo memo = (ShoppingMemo)parent.getItemAtPosition(position);
+            ShoppingMemo memo = (ShoppingMemo) parent.getItemAtPosition(position);
             ShoppingMemo updatedMemo = dataSource.updateShoppingMemo(memo.getId(), memo.getProduct(), memo.getQuantity(), (!memo.isChecked()));
             Log.d(TAG, "initializeShoppingMemoListView: Checked-Status von Eintrag: " + updatedMemo.toString() + " ist: " + updatedMemo.isChecked());
             showAllListEntries();
@@ -331,5 +343,82 @@ public class MainActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void startSacan(View view) {
+
+        Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+        intent.putExtra("SCAN_MODE", "PRODUCT_MODE");
+        try {
+            startActivityForResult(intent, 1);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(this, "Scanner nicht installiert", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            TextView product = findViewById(R.id.editText_product);
+            product.setText(getProductName(data.getStringExtra("SCAN_RESULT")));
+            TextView quantity = findViewById(R.id.editText_quantity);
+            quantity.requestFocus();
+        }else {
+            Toast.makeText(this, "Scan nicht möglich", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private String getProductName(String scanResult) {
+        HoleDatenTask task = new HoleDatenTask();
+        String result = null;
+        try {
+            result = task.execute(scanResult).get();
+            JSONObject rootObject = new JSONObject(result);
+            Log.d(TAG, "getProductName: " + rootObject.toString(2));
+            if (rootObject.has("product")) {
+                JSONObject productObject = rootObject.getJSONObject("product");
+                if (productObject.has("product_name")) {
+                    return productObject.getString("product_name");
+                }
+            }
+        } catch (InterruptedException e) {
+            Log.e(TAG, "", e);
+        } catch (ExecutionException e) {
+            Log.e(TAG, "", e);
+        } catch (JSONException e) {
+            Log.e(TAG, "", e);
+        }
+        return "Artikel nicht gefunden";
+    }
+
+
+    public class HoleDatenTask extends AsyncTask<String, Void, String> {
+
+
+        @Override
+        protected String doInBackground(String... strings) {
+            final String baseUrl = "https://world.openfoodfacts.org/api/v0/product/";
+            final String requestUrl = baseUrl + strings[0] + ".json";
+            Log.d(TAG, "doInBackground: " + requestUrl);
+            StringBuilder result = new StringBuilder();
+            URL url = null;
+
+            try {
+                url = new URL(requestUrl);
+            } catch (MalformedURLException e) {
+                Log.e(TAG, "", e);
+            }
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openConnection().getInputStream()))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    result.append(line);
+                }
+
+            } catch (IOException e) {
+
+            }
+            Log.d(TAG, "doInBackground: " + result.toString());
+            return result.toString();
+        }
     }
 }
